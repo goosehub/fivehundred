405b79e8b69885f6115f58445eb3926a692a3d2e
Goose <goosepostbox@gmail.com>
2016-05-24 23:04:09 -0400

    Optimized page and create form and logic
    Not functional.

--- application/views/master.php
+++ application/views/master.php
@@ -22,19 +22,39 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	&lt;/head&gt;
 	&lt;body&gt;
 
+	&lt;button&gt;
+		<a id="new">New</a>
+		<form action="new">
+			<label for="">Code</label>
+			<textarea name="code"></textarea>
+			<label pattern="[a-zA-Z0-9] for="">Title</label>
+			<input type="text" name="title"/>
+			<label for="">Language</label>
+			<select name="language">
+				<option value="example">Example</option>
+			</select>
+			<label for="">Syntax</label>
+			<select name="syntax">
+				<option value="example">Example</option>
+			</select>
+			<label for="">Submit</label>
+			<input type="submit" value="Submit"/>
+		</form>
+	&lt;/button&gt;
+
 	&lt;pre&gt;&lt;code&gt;
 		<a href="example">example</a>
 	&lt;/code&gt;&lt;/pre&gt;
 
 	&lt;script src=&quot;resources/libraries/jquery-2.2.0.min.js&quot;&gt;&lt;/script&gt;
 	&lt;script src=&quot;resources/libraries/highlightjs-9.4.0.min.js&quot;&gt;&lt;/script&gt;
-	&lt;script&gt;hljs.initHighlightingOnLoad();&lt;/script&gt;
+	&lt;script src=&quot;resources/script.js&quot;&gt;&lt;/script&gt;
 	&lt;/body&gt;
 	&lt;/html&gt;
 </code></pre>
 
 <script src="resources/libraries/jquery-2.2.0.min.js"></script>
 <script src="resources/libraries/highlightjs-9.4.0.min.js"></script>
-<script>hljs.initHighlightingOnLoad();</script>
+<script src="resources/script.js"></script>
 </body>
 </html>
---
+++ resources/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    console.log('hello world');
+    
+    hljs.initHighlightingOnLoad();
+
+    $('form').hide();
+    var form_on = false;
+    $('body').on('click', '#create', function() {
+        if (form_on) {
+            form_on = false;
+            $('form').hide();
+        } else {
+            form_on = true;
+            $('form').show();
+        }
+    });
+
+});
