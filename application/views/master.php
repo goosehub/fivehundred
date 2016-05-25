eb601f5a428fb78eb92e5c7bfa1b3ecefbe1eae9
Goose <goosepostbox@gmail.com>
2016-05-24 22:20:09 -0400

    Homepage meta
    Homepage meta

--- application/views/master.php
+++ application/views/master.php
@@ -5,37 +5,32 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
 	<meta charset="utf-8">
 	<title>Five Hundred Internal Server Error</title>
-
-	<!-- jQuery CDN -->
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-	<!-- highlighter.js CDN -->
-	<!-- Primary -->
 	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.4.0/styles/default.min.css">
 	<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.4.0/highlight.min.js"></script>
-	<!-- Alt -->
-	<!-- <link rel="stylesheet" href="//cdn.jsdelivr.net/highlight.js/9.4.0/styles/default.min.css"> -->
-	<!-- <script src="//cdn.jsdelivr.net/highlight.js/9.4.0/highlight.min.js"></script> -->
-
+	<link rel="stylesheet" href="resources/styles/dark.css">
+	<link rel="stylesheet" href="resources/style.css">
 </head>
 <body>
+<pre><code id="code" class="code">
+	&lt;!DOCTYPE html&gt;
+	&lt;html lang=&quot;en&quot;&gt;
+	&lt;head&gt;
+		&lt;meta charset=&quot;utf-8&quot;&gt;
+		&lt;title&gt;Five Hundred Internal Server Error&lt;/title&gt;
+		&lt;script src=&quot;//cdnjs.cloudflare.com/ajax/libs/jquery/2.2.0/jquery.min.js&quot;&gt;&lt;/script&gt;
+		&lt;link rel=&quot;stylesheet&quot; href=&quot;//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.4.0/styles/default.min.css&quot;&gt;
+		&lt;script src=&quot;//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.4.0/highlight.min.js&quot;&gt;&lt;/script&gt;
+		&lt;link rel=&quot;stylesheet&quot; href=&quot;resources/styles/dark.css&quot;&gt;
+		&lt;link rel=&quot;stylesheet&quot; href=&quot;resources/style.css&quot;&gt;
+	&lt;/head&gt;
+	&lt;body&gt;
 
-<!-- Page content -->
-<pre><code id="code" class="html">
-	&lt;html&gt; Five Hundred Internal Server_Error &lt;/html&gt;
+	&lt;script&gt;hljs.initHighlightingOnLoad();&lt;/script&gt;
+	&lt;/body&gt;
+	&lt;/html&gt;
 </code></pre>
 
-<!-- Init Highlighter JS -->
-<script>
-	$(document).ready(function() {
-	  $('pre code').each(function(i, block) {
-	    hljs.highlightBlock(block);
-	    name = 'html';
-	    value = 'dracula';
-	    ignore_illegals = true;
-	    hljs.highlight(name, value, ignore_illegals);
-	  });
-	});
-</script>
-
+<script>hljs.initHighlightingOnLoad();</script>
 </body>
 </html>
