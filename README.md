7e383a0f8d98671a9a0e07ae9fa4f1a3fe126f32
Goose <goosepostbox@gmail.com>
2016-05-25 01:25:44 -0400

    README and sql cleanup
    End of the first night.

---
+++ README.md
@@ -0,0 +1,7 @@
+<h1>Five Hundred Internal Server Error</h1>
+
+<ul>
+    <li>Put this code somewhere</li>
+    <li>Put stuff in application/config/database.php</li>
+    <li>Put fivehundred.sql in the database</li>
+</ul>
--- fivehundred.sql
+++ fivehundred.sql
@@ -1,30 +1,5 @@
--- phpMyAdmin SQL Dump
--- version 4.1.14
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: May 25, 2016 at 06:39 AM
--- Server version: 5.6.17
--- PHP Version: 5.5.12
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `fivehundred`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `code`
---
 
 CREATE TABLE IF NOT EXISTS `code` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -33,7 +8,3 @@ CREATE TABLE IF NOT EXISTS `code` (
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
